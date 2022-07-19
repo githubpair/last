18a18978e9dca6ec3934364aa02aab5e378dac43
MOONJEAYOON <jejusopp@lguplus.co.kr>
2022-07-19 16:59:45 +0900

    Initial commit

---
+++ src/main/java/com/example/last/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.last.controller;
+
+import com.example.last.dto.MemberRequestDto;
+import com.example.last.dto.MemberResponseDto;
+import com.example.last.dto.TokenDto;
+import com.example.last.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto requestDto) {
+        return ResponseEntity.ok(authService.signup(requestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto requestDto) {
+        return ResponseEntity.ok(authService.login(requestDto));
+    }
+}
---
+++ src/main/java/com/example/last/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.last.repository;
+
+import com.example.last.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
